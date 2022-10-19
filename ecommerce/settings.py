037031930b075570e325267c193a7f2da988e46c
Amit <amit170111nov@gmail.com>
2022-10-19 15:45:13 +0530

    Added Allowed host for Aws

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['ecommerce-env.eba-vzfikm92.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['ecommerce-project-env.eba-nnm3pmia.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
